c958eaf8c9559f379399a296f02c102a637d1688
Kawona  Oje <kawona.Oje@EURACARE.COM.GH>
2025-09-06 02:27:23 +0000

    Added 3-nav_index.html file

--- 0x02-tailwind-css/tailwind.config.js
+++ 0x02-tailwind-css/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./*.{html,js}",          // match ALL html/js files in project root
+    "./src/**/*.{html,js}",   // match everything inside src
+    "./dist/**/*.{html,js}",  // (optional) if you keep html in dist later
+  ],
   theme: {
     extend: {},
   },
